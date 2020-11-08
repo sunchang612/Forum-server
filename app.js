1dd1b42fc021f4409694358e90ab905b2b27f7a2
sunchang612 <sunchang612@gmail.com>
2020-11-08 23:57:49 +0800

    :tada: init

---
+++ app.js
@@ -0,0 +1,52 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const app = new Koa()
+const router = new Router()
+
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const debug = require('debug')('koa2:server')
+const path = require('path')
+
+const config = require('./config')
+const routes = require('./routes')
+
+const port = process.env.PORT || config.port
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser())
+  .use(json())
+  .use(logger())
+  .use((ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*'); //允许通过所有的 
+    next()
+  })
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - $ms`)
+})
+
+router.get('/', async (ctx, next) => {
+  ctx.body = 'Hello World'
+})
+
+routes(router)
+app.on('error', function(err, ctx) {
+  console.log(err)
+  logger.error('server error', err, ctx)
+})
+
+module.exports = app.listen(config.port, () => {
+  console.log(`Listening on http://localhost:${config.port}`)
+})
